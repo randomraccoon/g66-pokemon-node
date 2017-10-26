fdfca495689534fa8ea40f83323b1cdcab30cfde
Cameron Earl <developer.cameron@gmail.com>
2017-10-26 11:13:03 -0700

    Fixed seeds

--- db/seeds/0_trainers.js
+++ db/seeds/0_trainers.js
@@ -1,13 +1,13 @@
-
 exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+  return Promise.join(
+    // Deletes ALL existing entries
+    knex('trainers').del(),
+
+    // Inserts seed entries
+    knex('trainers').insert({id: 1, name: "Markel" }),
+    knex('trainers').insert({id: 2, name: "Jacob" }),
+    knex('trainers').insert({id: 3, name: "Ella" }),
+    knex('trainers').insert({id: 4, name: "Valencia" })
+
+  );
 };
