2614f47e17d9baff5fc239fe632bfb23dc5a476b
Cameron Earl <developer.cameron@gmail.com>
2017-10-28 18:29:35 -0700

    Still trying to fix heroku/knex error

---
+++ db/migrations/20171026103610_pokemon.js
@@ -0,0 +1,25 @@
+exports.up = (knex, Promise) => {
+  return knex.schema.createTable('pokemon', table => {
+    table.increments();
+    table.string('name').notNullable();
+    table.integer('species_id')
+     .notNullable()
+     .references('id')
+     .inTable('species')
+     .onDelete('CASCADE')
+     .index();
+    table.integer('trainer_id')
+     .notNullable()
+     .references('id')
+     .inTable('trainers')
+     .onDelete('CASCADE')
+     .index();
+    table.integer('cp').notNullable();
+    table.boolean('in_gym').notNullable().defaultTo('false');
+    table.timestamps(true, true);
+  });
+};
+
+exports.down = (knex, Promise) => {
+  return knex.schema.dropTable('pokemon');
+};
